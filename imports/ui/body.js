dd9f02442d1725a772183b48a3ea17b393a9a523
Chukwuemeka <chukwuemekandukwe01@gmail.com>
2018-09-18 06:42:45 +0100

    created form and add create feature

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,35 @@
 import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+import { Tasks } from '../api/tasks.js';
  
 import './body.html';
  
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  tasks() {
+    return Tasks.find({});
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    console.log('Go EFF yourself')
+    // Prevent default browser form submit
+    // event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const firstname = target.firstname.value;
+    const lastname = target.lastname.value;
+    const gender = target.gender.value;
+    const dob = target.dob.value;
+ 
+    Meteor.call('tasks.insert', firstname, lastname, gender, dob);
+
+ 
+    // Clear form
+    target.firstname.value = '';
+    target.lastname.value = '';
+    target.gender.value = '';
+    target.dob.value = '';
+  },
 });
